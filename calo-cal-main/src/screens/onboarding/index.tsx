c678bf551862ed9a6e90c2c42e2b9da71819ee41
Adem Nasr <122630068+adamnaser1@users.noreply.github.com>
2026-03-09 00:32:27 +0100

    Delete calo-cal-main directory

--- calo-cal-main/src/screens/onboarding/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as WelcomeScreen } from './WelcomeScreen';
-export { default as GoalScreen } from './GoalScreen';
-export { default as ProfileScreen } from './ProfileScreen';
-export { default as CalorieTargetScreen } from './CalorieTargetScreen';
